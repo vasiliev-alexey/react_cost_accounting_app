c5cf6689e202f951c4c8965daa84232a12dfeb52
vasiliev-alexey <alex_v81@MAIL.RU>
2021-08-06 15:56:38 +0300

    add login func

--- src/ts/ui/Header/Header.tsx
+++ src/ts/ui/Header/Header.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router';
 
-export class Header extends React.Component<{}, {}> {
+class Header extends React.Component<RouteComponentProps, {}> {
+  #login = () => {
+    this.props.history.push('/sign-in');
+  };
+
   render(): React.ReactElement {
     return (
       <div>
-        <Button data-testid={'HeaderLoginButton'} variant="outline-info">
+        <Button
+          data-testid={'HeaderLoginButton'}
+          variant="outline-info"
+          onClick={this.#login}
+        >
           Login
         </Button>
       </div>
     );
   }
 }
+
+export default withRouter(Header);
